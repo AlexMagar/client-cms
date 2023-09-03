ddf72fe53a762f1acb9b6fca40c5474a9015944c
AlexMagar <lx.mgr07@gmail.com>
2023-09-03 20:05:50 +1000

    added route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import { ToastContainer } from "react-toastify";
 import { Header } from "./components/layout/Header";
 import { Footer } from './components/layout/Footer';
+import { Route, Routes } from "react-router-dom";
+import { Home } from './pages/home/Home'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <p>Hello</p>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
      <ToastContainer />
     </div>
   );
