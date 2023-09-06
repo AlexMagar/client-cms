8aa1ba381bb5e4111d3ce9ab1307e1196ce00ff8
AlexMagar <lx.mgr07@gmail.com>
2023-09-06 22:16:00 +1000

    carousel added

--- src/components/Hero/CarouselItems.js
+++ src/components/Hero/CarouselItems.js
@@ -1,12 +1,12 @@
 import { Button, Paper } from '@mui/material'
 import React from 'react'
 
-export const CarouselItems = (props) => {
+export const CarouselItems = (item) => {
   return (
     <div className=' d-flex'>
     <Paper >
-        <h2>{props.item.name}</h2>
-        <p>{props.item.description}</p>
+        <img src={item.image} alt={item.title} style={{width:'100%', height:'45vh'}}/>
+        <h2>{item.title}</h2>
         <Button className='checkButton'>Check it out!!</Button>
     </Paper>
     </div>
