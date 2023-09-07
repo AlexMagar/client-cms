96af6a222d50363f23e1ca922c4fbe52493d1cb8
AlexMagar <lx.mgr07@gmail.com>
2023-09-07 23:58:00 +1000

    carousel button is removed

--- src/components/Hero/CarouselItems.js
+++ src/components/Hero/CarouselItems.js
@@ -5,9 +5,8 @@ export const CarouselItems = (item) => {
   return (
     <div className=' d-flex'>
     <Paper >
-        <img src={item.image} alt={item.title} style={{width:'100%', height:'45vh'}}/>
-        <h2>{item.title}</h2>
-        <Button className='checkButton'>Check it out!!</Button>
+        <img src={item.item.image} alt={item.item.title} style={{width:'100%', height:'45vh'}}/>
+        <h2>{item.item.title}</h2>
     </Paper>
     </div>
   )
