3cd89b2fce1405509a207231137bf85b6abf3e29
AlexMagar <lx.mgr07@gmail.com>
2023-09-07 23:58:58 +1000

    new card view is added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Header } from '../../components/layout/Header'
 import { Footer } from '../../components/layout/Footer'
 import { Hero } from '../../components/Hero/Hero'
+import { CardView } from '../../components/CustomCard/CardView'
 
 export const Home = () => {
   return (
     <div>
         <Header />
         <main className='main'>
-            <Hero />
+          <Hero />
+          <CardView />
         </main>
         <Footer />
     </div>
