3931ba2f99f2878777e243d2d97b738764f3b89c
AlexMagar <lx.mgr07@gmail.com>
2023-09-09 18:59:04 +1000

    carousel item

--- src/components/Hero/CarouselItems.js
+++ src/components/Hero/CarouselItems.js
@@ -5,7 +5,7 @@ export const CarouselItems = (item) => {
   return (
     <div className=' d-flex'>
     <Paper >
-        <img src={item.item.image} alt={item.item.title} style={{width:'100%', height:'45vh'}}/>
+        <img src={item.item.image} alt={item.item.title} style={{width:'100%', height:'50vh'}}/>
         <h2>{item.item.title}</h2>
     </Paper>
     </div>
