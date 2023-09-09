631a3f0e97fb5d35fafdd26e4d6304f2f073a61f
AlexMagar <lx.mgr07@gmail.com>
2023-09-09 18:59:34 +1000

    custom margin adde to header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -87,7 +87,7 @@ export const Header = () => {
         console.log("Cart")
     }
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{marginBottom:2}}>
           <Container maxWidth="xl">
             <Toolbar disableGutters>
               <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
