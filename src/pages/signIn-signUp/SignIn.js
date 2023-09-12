7b4677b41f5db0d5d63ed69160d0ec30339796a0
AlexMagar <lx.mgr07@gmail.com>
2023-09-12 23:53:38 +1000

    new page signIN

---
+++ src/pages/signIn-signUp/SignIn.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Box from "@mui/material/Box";
+import { TextField } from '@mui/material';
+import { Header } from '../../components/layout/Header';
+import { Footer } from '../../components/layout/Footer';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export const SignIn = () => {
+
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+        <Header />
+        <Box >
+            <TextField id="outlined-basic" label="User Name" variant='outlined'/>        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl>
+        </Box>
+        <Footer />
+    </div>
+  )
+}
