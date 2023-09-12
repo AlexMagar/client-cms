2bd1abe6deac930c0e4791edbd4281fe26950eb9
AlexMagar <lx.mgr07@gmail.com>
2023-09-12 23:53:25 +1000

    new route signin

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Header } from "./components/layout/Header";
 import { Footer } from './components/layout/Footer';
 import { Route, Routes } from "react-router-dom";
 import { Home } from './pages/home/Home'
+import { SignIn } from './pages/signIn-signUp/SignIn';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/signIn' element={ <SignIn />} />
       </Routes>
      <ToastContainer />
     </div>
