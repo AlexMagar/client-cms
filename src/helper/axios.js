72fc27c3762b14709a18e2d2a3a85db746dcfd0e
AlexMagar <lx.mgr07@gmail.com>
2023-09-16 23:43:13 +1000

    degub

--- src/helper/axios.js
+++ src/helper/axios.js
@@ -1,3 +1,4 @@
+
 import axios from 'axios'
 
 const rootAPI = process.env.REACT_APP_ROOTAPI
@@ -5,14 +6,14 @@ const adminAPI = rootAPI + '/admin'
 
 const axiosProcessor = async ({method, url, obj, isPrivate}) =>{
 
+    // const headers = {
+    //     Authorization: isPrivate ? token : null
+    // }
     try {
-
-        console.log("axios", obj)
         const {data} = await axios({
             method,
             url,
-            data: obj
-
+            data: obj,
         })
     } catch (error) {
         return {
@@ -29,20 +30,16 @@ export const getAdminInfo = () =>{
         url: adminAPI,
         isPrivate: true
     }
-    console.log("getAdminInfo:", obj)
     return axiosProcessor(obj)
 }
-
-
 
 // ============ product info ============
-
-export const getProduct = () =>{
-    const obj = {
-        method: 'get',
-        url: adminAPI,
-        isPrivate: true
-    }
-    return axiosProcessor(obj)
-}
+// export const getProduct = () =>{
+//     const obj = {
+//         method: 'get',
+//         url: adminAPI,
+//         isPrivate: true
+//     }
+//     return axiosProcessor(obj)
+// }
 
