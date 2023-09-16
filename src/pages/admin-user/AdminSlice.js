b0378c75f234118614c9742b922713029cbe5bd2
AlexMagar <lx.mgr07@gmail.com>
2023-09-16 23:43:35 +1000

    created new slice for admin

---
+++ src/pages/admin-user/AdminSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+
+const initialState = {
+    user:[]
+}
+
+const adminSlice = createSlice({
+    name: 'admin',
+    initialState,
+    reducers:{
+        setAdmin: (state, {payload}) => {
+            state.admin = payload
+        }
+    }
+})
+
+const { reducer, actions} = adminSlice
+export const { setAdmin} = actions;
+export default reducer;
