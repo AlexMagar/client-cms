bb97227bf46ead38ff1e79200df33c6356eee0d3
AlexMagar <lx.mgr07@gmail.com>
2023-09-16 23:42:37 +1000

    added adminInfo-> reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./pages/product/productSlice";
-
+import adminReducer from "./pages/admin-user/AdminSlice";
 
 const store = configureStore({
     reducer: {
-        productInfo: productReducer
+        productInfo: productReducer,
+        adminInfo: adminReducer,
     }
 })
 
