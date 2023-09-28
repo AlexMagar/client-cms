376c9a2aba7e2968cb16cbe737dc297856775868
AlexMagar <lx.mgr07@gmail.com>
2023-09-28 23:27:02 +1000

    useEffect added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 
 import './App.css';
 import { ToastContainer } from "react-toastify";
-import { Header } from "./components/layout/Header";
-import { Footer } from './components/layout/Footer';
 import { Route, Routes } from "react-router-dom";
 import { Home } from './pages/home/Home'
 import 'react-toastify/dist/ReactToastify.css';
 import { SignIn } from './pages/signIn-signUp/SignIn';
+import { Product } from './pages/product/Product';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getProductAction } from './pages/product/productAction';
 
 function App() {
+
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getProductAction())
+  },[dispatch])
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/signIn' element={ <SignIn />} />
+        <Route path='/product' element={ <Product />} />
       </Routes>
      <ToastContainer />
     </div>
