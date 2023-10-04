c3022fb7632268aee359a68579c0678ce3dd225b
AlexMagar <lx.mgr07@gmail.com>
2023-10-04 22:37:09 +1100

    useState to hold the form data

--- src/pages/signIn-signUp/NewAccount.js
+++ src/pages/signIn-signUp/NewAccount.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '../../components/layout/Header'
 import { Footer } from '../../components/layout/Footer'
 import { Form } from 'react-bootstrap'
 import { Box, Button, Stack, TextField } from '@mui/material'
 
+
+const initialState = {
+    firstName: "",
+    email: ""
+}
+
 export const NewAccount = () => {
+
+    const [form, setForm] = useState(initialState)
+
+
+    const handleOnChange = (e) => {
+        const {name, value} = e.target
+
+        setForm({
+            ...form,
+            [name]:value
+        })
+    }
 
     const handleOnSubmit =(e) => {
         e.preventDefault()
-        const {name, value} = e.target
 
-        console.log(name, value)
+        console.log(form)
     }
 
   return (
@@ -19,8 +36,8 @@ export const NewAccount = () => {
         <Form onSubmit={handleOnSubmit}>
             <Stack spacing={2} direction='column' width='100%' alignItems='center'>
                 <h3>Create New Account</h3>
-                <TextField label='First Name' type='text' name='firstName'/>
-                <TextField label='Email' type='email' name='email'/>
+                <TextField label='First Name' type='text' name='firstName' onChange={handleOnChange}/>
+                <TextField label='Email' type='email' name='email' onChange={handleOnChange}/>
                 <Button type='submit' variant='contained' sx={{width:'200px'}}>Register</Button>
             </Stack>
         </Form>
