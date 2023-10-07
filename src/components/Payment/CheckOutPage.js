8e85f3079c9b219ae5b5330514fb69fd52e65667
AlexMagar <lx.mgr07@gmail.com>
2023-10-07 23:00:01 +1100

    andle event handler

--- src/components/Payment/CheckOutPage.js
+++ src/components/Payment/CheckOutPage.js
@@ -1,11 +1,36 @@
-import React from 'react'
-import { PaymentElement } from "@stripe/react-stripe-js";
+import React, { useState } from 'react'
+import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 export const CheckOutPage = () => {
+
+  const stripe = useStripe()
+  const elements = useElements()
+  const [form, setForm] = useState({})
+
+  const handleOnChange = (e) =>{
+    const {name, value} = e.target
+
+    setForm({
+      ...form,
+      [name]: value
+    })
+
+  }
+
+  const handleOnSubmit = e =>{
+    e.preventDefault()
+    //you should have user and other details
+    if(!stripe ||  !elements){
+      return
+    }
+
+  }
+
   return (
-    <form>
-        <input type='text'/>
-        <PaymentElement />
+    <form onSubmit={handleOnSubmit}>
+      <p>name: <input onChange={handleOnChange} type='text' name='name' /></p>
+      <p>email: <input onChange={handleOnChange} type='email' name='email'/></p>
+        <PaymentElement options={{hidePostalCode: true}}/>
         <button>Submit</button>
     </form>
   )
