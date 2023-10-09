2107aaeefd7a5f8fa904d6b1d38010793ff0bfdd
AlexMagar <lx.mgr07@gmail.com>
2023-10-09 22:37:40 +1100

    get client secret

--- src/components/Payment/CheckOutPage.js
+++ src/components/Payment/CheckOutPage.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
-import { PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { CardElement, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from 'axios'
 
 export const CheckOutPage = () => {
 
   const stripe = useStripe()
   const elements = useElements()
   const [form, setForm] = useState({})
+
+  const cart = {
+    amount: 25,
+    name: 'pen box'
+  }
 
   const handleOnChange = (e) =>{
     const {name, value} = e.target
@@ -17,11 +23,46 @@ export const CheckOutPage = () => {
 
   }
 
-  const handleOnSubmit = e =>{
+  const handleOnSubmit = async (e) =>{
     e.preventDefault()
     //you should have user and other details
     if(!stripe ||  !elements){
       return
+    }
+
+    //call our api server to get the client secret
+    try {
+      const apiUrl = "http://localhost:8000/payment"
+      const {data} = await axios.post(apiUrl, {
+        amount: cart.amount,
+        currency: 'aud',
+        PaymentMethodType: 'card'
+      })
+      const clientSecret = data.clientSecret
+
+      const {paymentIntent} = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: form.name,
+            email: form.email
+          }
+        }
+      })
+
+      if(paymentIntent.status === 'succeeded'){
+        alert('Your order has been successfully')
+
+        //call your order server to create new order in the db
+      }else{
+        alert("couldn't process the payment, try again later")
+      }
+
+      //confirming the payment to strip server
+
+
+    } catch (error) {
+      console.log(error)
     }
 
   }
