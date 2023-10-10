155ebe725920a7dd4c94bc1e896049782b92380e
AlexMagar <lx.mgr07@gmail.com>
2023-10-10 21:53:33 +1100

    debug

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -5,21 +5,22 @@ import { CheckOutPage } from './CheckOutPage';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(process.env.REACT_STRIPE_PUBLISHABLE_KEY)
+// const stripePromise = loadStripe(process.env.REACT_STRIPE_PUBLISHABLE_KEY)
+const stripePromise = loadStripe('pk_test_51NyC3uBFfEx6yZmLeKKtboOurMVOzjgC2kzhfFUSsOLb899h34OAxuTT8sm93uKa5oFcS8sl7sxHisfpr5cvKs8o00FOHPC0qh')
 console.log('API key stripe:', stripePromise)
 
 export const Payment = () => {
 
-    const options = {
-        //passing the client secret ovtained from the server
-        clientSecret: '{{CLIENT_SECRET}}'
-    }
+    // const options = {
+    //     //passing the client secret ovtained from the server
+    //     clientSecret: '{{CLIENT_SECRET}}'
+    // }
 
   return (
     <div>
         <h3>Payment</h3>
         <hr/>
-        <Elements stripe={stripePromise} options={options}>
+        <Elements stripe={stripePromise}>
             <CheckOutPage />
         </Elements>
     </div>
