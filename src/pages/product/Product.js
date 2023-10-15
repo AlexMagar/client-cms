562780b9ab2dbe1b96e38fff78b1ff37a0e97d5f
AlexMagar <lx.mgr07@gmail.com>
2023-10-15 23:10:50 +1100

    get the image to display

--- src/pages/product/Product.js
+++ src/pages/product/Product.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
 import { getProductAction } from './productAction'
 import { useDispatch, useSelector } from 'react-redux'
+import { ImageList, ImageListItem } from '@mui/material'
 
 export const Product = () => {
 
     const dispatch = useDispatch()
 
     const { products } = useSelector((state) => state.productInfo)
+
+    console.log("This is from the product itself, ", products)
 
     useEffect(() => {
         dispatch(getProductAction())
@@ -16,7 +19,21 @@ export const Product = () => {
     <div>
         <p>Product</p>
         <hr/>
-        <p>This many product are there {products.length}</p>
+        <p>This many products are there {products.length}</p>
+        <ImageList sx={{width: 500, height: 450}} cols={3} rowHeight={164}>
+          {
+            products.map((item,i) => (
+              <ImageListItem key={item.thumbnail}>
+                <img 
+                  srcSet={`${item.thumbnail}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.thumbnail}?w=164&h=164&fit=crop&auto=format`}
+                  alt={item.name}
+                  loading='lazy'
+                />
+              </ImageListItem>
+            ))
+          }
+        </ImageList>
     </div>
   )
 }
